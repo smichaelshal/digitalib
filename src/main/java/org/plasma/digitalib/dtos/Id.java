b62298dfecf2566cc82355f54540fa6c635f87f4
unknown <mshalitin@gmail.com>
2024-08-08 10:28:19 +0300

    make Id to Serializable

--- src/main/java/org/plasma/digitalib/dtos/Id.java
+++ src/main/java/org/plasma/digitalib/dtos/Id.java
@@ -3,10 +3,11 @@ package org.plasma.digitalib.dtos;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @RequiredArgsConstructor
 @Getter
-public class Id {
+public class Id implements Serializable {
     protected final UUID id;
 }
