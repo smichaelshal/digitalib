8a90a2734a8d5eface8bdf1e0fef55609af8b7ab
unknown <mshalitin@gmail.com>
2024-08-08 11:19:04 +0300

    add Storage interface

---
+++ src/main/java/org/plasma/digitalib/storage/Storage.java
@@ -0,0 +1,10 @@
+package org.plasma.digitalib.storage;
+
+import java.util.List;
+import java.util.function.Function;
+
+public interface Storage <T> {
+    boolean create(T item);
+    List<T> readAll(Function<T, Boolean> filter);
+    void update(T oldItem, T newItem);
+}
