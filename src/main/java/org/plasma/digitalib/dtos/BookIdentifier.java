e51194120c0f2a81acc13fe62bf3118193b93717
unknown <mshalitin@gmail.com>
2024-08-08 10:50:29 +0300

    add BookIdentifier

--- src/main/java/org/plasma/digitalib/dtos/BookIdentifier.java
+++ src/main/java/org/plasma/digitalib/dtos/BookIdentifier.java
@@ -3,9 +3,11 @@ package org.plasma.digitalib.dtos;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.io.Serializable;
+
 @Getter
 @RequiredArgsConstructor
-public class BookIdentifier {
+public class BookIdentifier implements Serializable {
     private final String name;
     private final String author;
 }
