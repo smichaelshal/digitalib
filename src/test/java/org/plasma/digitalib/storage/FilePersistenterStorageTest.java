1810621128002ffe8154bd2af45a2c558dd98f7f
unknown <mshalitin@gmail.com>
2024-08-14 10:16:01 +0300

    fix tests

--- src/test/java/org/plasma/digitalib/storage/FilePersistenterStorageTest.java
+++ src/test/java/org/plasma/digitalib/storage/FilePersistenterStorageTest.java
@@ -6,19 +6,25 @@ import org.plasma.digitalib.dtos.BookIdentifier;
 import org.plasma.digitalib.dtos.Borrowing;
 import org.plasma.digitalib.dtos.User;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.apache.commons.lang3.SerializationUtils;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FilePersistenterStorageTest {
 
-    private Storage<Book> createStorage() {
-        // create emptry tmp directory <<<
-        Path path = Path.of("C:\\Users\\Ori\\IdeaProjects\\digitalib\\src\\db");
+    private Storage<Book> createStorage() throws IOException {
+        Path path = Files.createTempDirectory(UUID.randomUUID().toString());
         return new FilePersistenterStorage<>(new LinkedList<Book>(), path);
     }
 
@@ -30,15 +36,15 @@ class FilePersistenterStorageTest {
     }
 
     @Test
-    public void addBookTest() {
+    public void addBookTest() throws IOException {
         Storage<Book> storage = this.createStorage();
         Book book = this.createBook();
         storage.create(book);
 
-//        AnyBookFilter anyBookFilter = new AnyBookFilter();
-        BookByIdFilter bookByIdFilter = new BookByIdFilter(book.getId());
+        AnyBookFilter anyBookFilter = new AnyBookFilter();
+//        BookByIdFilter bookByIdFilter = new BookByIdFilter(book.getId());
 
-        List<Book> books = storage.readAll(bookByIdFilter);
+        List<Book> books = storage.readAll(anyBookFilter);
         assertEquals(1, books.size());
 
         Book bookFromStorage = books.get(0);
@@ -51,24 +57,25 @@ class FilePersistenterStorageTest {
     }
 
     @Test
-    public void updateBookTest() {
+    public void updateBookTest() throws IOException {
         Storage<Book> storage = this.createStorage();
         Book book = this.createBook();
         storage.create(book);
 
-        Instant expiredTime = Instant.now().plus(1000, ChronoUnit.SECONDS);
-        book.getBorrowings().add(new Borrowing(new User("5678"), Instant.now(), expiredTime));
+        Book bookCopy = SerializationUtils.clone(book);
 
-        // fix deep copy to test object
-        storage.update(book, book);
+        Instant expiredTime = Instant.now().plus(1000, ChronoUnit.SECONDS);
+        Borrowing borrowing = new Borrowing(new User("5678"), Instant.now(), expiredTime);
+        bookCopy.getBorrowings().add(borrowing);
+        storage.update(book, bookCopy);
 
         BookByIdFilter bookByIdFilter = new BookByIdFilter(book.getId());
 
         List<Book> books = storage.readAll(bookByIdFilter);
         // fix check if size > 0
 
-        Book bookFromStorage = books.get(0);
-        List<Borrowing> borrowings = book.getBorrowings();
+        Book bookResult = books.get(0);
+        List<Borrowing> borrowingsResult = bookResult.getBorrowings();
         // fix equal between borrowings
 //        assertEquals(borrowings.get(borrowings.size() - 1), bookFromStorage.getBookIdentifier());
     }
