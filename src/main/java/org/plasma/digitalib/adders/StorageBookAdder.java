0b691343d0f723c1a07d4c5574c5b01d4d511d9b
unknown <mshalitin@gmail.com>
2024-08-14 14:01:32 +0300

    fix StorageBookAdder

--- src/main/java/org/plasma/digitalib/adders/StorageBookAdder.java
+++ src/main/java/org/plasma/digitalib/adders/StorageBookAdder.java
@@ -1,9 +1,10 @@
-package org.plasma.digitalib.Adder;
+package org.plasma.digitalib.adders;
 
 import lombok.AllArgsConstructor;
 import org.plasma.digitalib.dtos.Book;
 import org.plasma.digitalib.storage.Storage;
 
+import java.util.List;
 import java.util.function.Function;
 
 @AllArgsConstructor
@@ -11,8 +12,14 @@ public class StorageBookAdder implements ItemAdder<Book> {
     private final Storage<Book> storage;
     private final Function<Book, Boolean> bookIdentifierFilter;
 
-    public final boolean add(Book book) {
-
+    public final boolean add(final Book book) {
+        List<Book> bookResults = this.storage.readAll(this.bookIdentifierFilter);
+        for (Book bookResult : bookResults) {
+            if (!bookResult.getSummary().equals(book.getSummary())
+                    || !bookResult.getGenre().equals(book.getGenre())) {
+                return false;
+            }
+        }
         return this.storage.create(book);
     }
 }
