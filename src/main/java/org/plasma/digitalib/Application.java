153c1ed415faa83ba744983b960a60287676f821
unknown <mshalitin@gmail.com>
2024-08-15 14:22:44 +0300

    clean Application

--- src/main/java/org/plasma/digitalib/Application.java
+++ src/main/java/org/plasma/digitalib/Application.java
@@ -1,15 +1,6 @@
 package org.plasma.digitalib;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-
 public final class Application {
     public static void main(String[] args) {
-        Logger logger = LoggerFactory.getLogger(Application.class);
-
-        logger.error("An exception occurred!");
-        logger.error("An exception occurred!", new Exception("Custom exception"));
-        logger.error("{}, {}! An exception occurred!", "Hello", "World", new Exception("Custom exception"));
     }
 }
