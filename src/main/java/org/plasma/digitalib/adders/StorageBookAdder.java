bee560412196c13983a9362809e0fc81955ed646
unknown <mshalitin@gmail.com>
2024-08-18 12:26:47 +0300

    fix filter and add support to empty fields

--- src/main/java/org/plasma/digitalib/adders/StorageBookAdder.java
+++ src/main/java/org/plasma/digitalib/adders/StorageBookAdder.java
@@ -2,6 +2,7 @@ package org.plasma.digitalib.adders;
 
 import lombok.AllArgsConstructor;
 import lombok.NonNull;
+import org.plasma.digitalib.filters.BookIdentifierFilter;
 import org.plasma.digitalib.models.Book;
 import org.plasma.digitalib.storage.Storage;
 
@@ -11,17 +12,29 @@ import java.util.function.Function;
 @AllArgsConstructor
 public class StorageBookAdder implements ItemAdder<Book> {
     private final Storage<Book> storage;
-    private final Function<Book, Boolean> bookIdentifierFilter;
 
     public final boolean add(@NonNull final Book book) {
         List<Book> bookResults = this.storage
-                .readAll(this.bookIdentifierFilter);
+                .readAll(
+                        new BookIdentifierFilter(book.getBookIdentifier()));
         for (Book bookResult : bookResults) {
-            if (!bookResult.getSummary().equals(book.getSummary())
-                    || !bookResult.getGenre().equals(book.getGenre())) {
-                return false;
+            if (!bookResult.getSummary().equals(book.getSummary())) {
+                if (book.getSummary().isEmpty()) {
+                    book.setSummary(bookResult.getSummary());
+                } else {
+                    return false;
+                }
+            }
+
+            if (!bookResult.getGenre().equals(book.getGenre())) {
+                if (book.getGenre().isEmpty()) {
+                    book.setGenre(bookResult.getGenre());
+                } else {
+                    return false;
+                }
             }
         }
+
         return this.storage.create(book);
     }
 }
