0809c2065fac9f8c68de9a1d0db7c11f665a8b47
unknown <mshalitin@gmail.com>
2024-08-18 11:47:25 +0300

    fix cr

--- src/main/java/org/plasma/digitalib/filters/IdsFilter.java
+++ src/main/java/org/plasma/digitalib/filters/IdsFilter.java
@@ -1,20 +1,21 @@
 package org.plasma.digitalib.filters;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.plasma.digitalib.models.BorrowableItem;
 
 import java.util.List;
 import java.util.UUID;
 import java.util.function.Function;
 
-public class IdFilter<T extends BorrowableItem>
+@Slf4j
+@AllArgsConstructor
+public class IdsFilter<T extends BorrowableItem>
         implements Function<T, Boolean> {
     private final List<UUID> ids;
 
-    public IdFilter(final List<UUID> uuids) {
-        this.ids = uuids;
-    }
-
     public final Boolean apply(final T item) {
+        log.info("{}, id: {} borrowed", item.getId().toString());
         return ids.contains(item.getId());
     }
 }
