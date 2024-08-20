6c675654d7b131fcec34501872efe534c6e4294b
unknown <mshalitin@gmail.com>
2024-08-20 11:22:50 +0300

    add PresentBookFilterTest and tests

---
+++ src/main/java/org/plasma/digitalib/filters/PresentBookFilter.java
@@ -0,0 +1,17 @@
+package org.plasma.digitalib.filters;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.plasma.digitalib.models.Book;
+
+import java.util.function.Predicate;
+
+@Slf4j
+@AllArgsConstructor
+public class PresentBookFilter implements Predicate<Book> {
+    public final boolean test(final Book book) {
+        boolean isPresent = !book.getIsBorrowed();
+        log.debug("{} is {}present", book, (isPresent ? "" : "not "));
+        return isPresent;
+    }
+}
