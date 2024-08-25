3554b91cfee57c77519532505863882e8454bc8b
unknown <mshalitin@gmail.com>
2024-08-25 12:06:22 +0300

    fix add copy book without summary and genre

--- src/main/java/org/plasma/digitalib/tasks/AddBookCopyTask.java
+++ src/main/java/org/plasma/digitalib/tasks/AddBookCopyTask.java
@@ -5,32 +5,25 @@ import org.plasma.digitalib.adders.ItemAdder;
 import org.plasma.digitalib.inputs.Input;
 import org.plasma.digitalib.models.Book;
 import org.plasma.digitalib.models.BookIdentifier;
+import org.plasma.digitalib.storage.Storage;
 
 public class AddBookCopyTask extends Task {
     private final ItemAdder<Book> adder;
     private final ConsoleCreatorBookIdentifier creatorBookIdentifier;
-    private final Input consoleUtils;
 
     public AddBookCopyTask(
             @NonNull final String name,
             @NonNull final ItemAdder<Book> adder,
-            @NonNull final ConsoleCreatorBookIdentifier creatorBookIdentifier,
-            @NonNull final Input consoleUtils) {
+            @NonNull final ConsoleCreatorBookIdentifier creatorBookIdentifier) {
         super(name);
         this.adder = adder;
         this.creatorBookIdentifier = creatorBookIdentifier;
-        this.consoleUtils = consoleUtils;
     }
 
     @Override
     public final void run() {
         BookIdentifier bookIdentifier = this.creatorBookIdentifier.create();
-        String genre = this.consoleUtils.getParameter(
-                "genre book");
-        String summary = this.consoleUtils.getParameter(
-                "summary book");
-
-        Book book = new Book(genre, summary, bookIdentifier);
+        Book book = new Book("", "", bookIdentifier);
         boolean adderResult = this.adder.add(book);
         if (adderResult) {
             System.out.println("success created book");
